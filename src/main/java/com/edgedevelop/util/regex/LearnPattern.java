d73c5f1a320e03f169af3273842140763305e33c
Ejerson Balabas <ejersonb@gmail.com>
2020-01-23 10:22:31 -0700

    learned how to check for invalid regex pattern and how to handle the exception that it causes

--- src/main/java/com/edgedevelop/util/regex/LearnPattern.java
+++ src/main/java/com/edgedevelop/util/regex/LearnPattern.java
@@ -1,11 +1,26 @@
 package com.edgedevelop.util.regex;
 
+import java.util.Scanner;
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
 public class LearnPattern {
 
     public static void main(String[] args) {
+        // https://www.hackerrank.com/challenges/pattern-syntax-checker/problem
 
+        // This checks if a regex pattern is valid or invalid
+        try {
+            Pattern.compile("([A-Z])(.+)");
+            System.out.println("Valid");
 
+        } catch (Exception e) {
+            System.out.println("Invalid");
+        }
 
     }
+
 
 }
